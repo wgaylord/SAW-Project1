f73dec58c39186c289918730bd637bcbc9faf80e
William <chibill110@gmail.com>
2020-09-24 16:44:37 -0500

    Work on Getting and sending changes. Have to work on error handling in the https get functions and need to write the compare function.

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,5 +1,19 @@
 var socket = io.connect('/');
 
-socket.on('message', function(data) {
-  console.log('Message received: ' + data);
+socket.on('init', function(data) {
+	console.log('Inital Data: '+data);
+	data.forEach(element => console.log(element));
+});
+
+
+/*
+* Data is sent as the new data plus a extra field for what fields changed
+* Ex: {"activator": "", "name": "", "reference": "", "locationDesc": "", 
+* "activityStart": "", "activityEnd": "", "startDate": "", "endDate": "",
+* "startTime": "", "endTime": "", "frequencies": "", "comments": "","changed":["frequencies"]}
+*/
+
+socket.on('changed', function(data) {
+  console.log('Change received: ' + data);
+  console.log('Fields that changed are: ' + data.changed);
 });
--- routes/index.js
+++ routes/index.js
@@ -7,23 +7,7 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  // Reference: https://nodejs.org/api/https.html#https_https_get_options_callback
-  const url = 'https://api.pota.us/activation'
-
-  https.get(url, function(response){
-    console.log(response.statusCode);
-
-    response.on('data', function(data){
-      const potaData = JSON.parse(data)
-      const activatorData = potaData[1].activator
-      const frequencyData = potaData[1].frequencies
-      //console.log(activatorData);
-      //console.log(frequencyData);
-      res.render('index', { title: 'The activator: ' + activatorData + ' online on frequency: ' + frequencyData });
-    });
-  });
-
-  //res.render('index', { title: 'Express' });
+  res.render('index',{ title: 'POTA Activations'});
 });
 
 module.exports = router;
