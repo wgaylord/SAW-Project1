f6df6e8063062f301e1bbe45d3656bcf53e50bbd
aaldossari <aaldossari@hawk.iit.edu>
2020-09-24 01:33:35 -0500

    Add callback function to fetch and parse data from Pota.us

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,29 @@
 'use strict';
 
 const express = require('express');
+const https = require('https');
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  // Reference: https://nodejs.org/api/https.html#https_https_get_options_callback
+  const url = 'https://api.pota.us/activation'
+
+  https.get(url, function(response){
+    console.log(response.statusCode);
+
+    response.on('data', function(data){
+      const potaData = JSON.parse(data)
+      const activatorData = potaData[1].activator
+      const frequencyData = potaData[1].frequencies
+      //console.log(activatorData);
+      //console.log(frequencyData);
+      res.render('index', { title: 'The activator: ' + activatorData + ' online on frequency: ' + frequencyData });
+    });
+  });
+
+  //res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
